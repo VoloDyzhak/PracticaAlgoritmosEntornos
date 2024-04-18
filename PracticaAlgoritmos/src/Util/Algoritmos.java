295f5ee890b10449cdb1924149843fa13e0aae15
VoloDyzhak <149478374+VoloDyzhak@users.noreply.github.com>
2024-04-18 20:43:06 +0200

    Realize main con bucles de arrray e implementacion de los metodos

--- PracticaAlgoritmos/src/Util/Algoritmos.java
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -1,44 +1,71 @@
 package Util;
 
+/**
+ * Clase abstracta de Algoritmo
+ * 
+ * @version 1.01
+ */
 public abstract class Algoritmos {
- private int numero;
- 
- public void fibonacci(int numero) {
-	    int a = 0;
-	    int b = 1;
+	private int numero;
 
+	/**
+	 * Funcion fibonacci Los primeros números de la serie, cuyo índice empieza en 0,
+	 * son 0 y 1. partir del segundo índice de la serie, para calcular el número de
+	 * la posición n, sumamos los dos anteriores. Por ejemplo, el fibonacci de 2 se
+	 * obtiene de sumar 0 y 1, que son los dos valores anteriores de la serie
+	 * 
+	 * @param numero
+	 */
+	public void fibonacci(int numero) {
+		int a = 0;
+		int b = 1;
 
-	    for (int i = 2; i < numero; i++) {
-	        int siguiente = a + b;
-	      
-	        a = b;
-	        b = siguiente;
-	    }
-	  
+		for (int i = 2; i < numero; i++) {
+			int siguiente = a + b;
+			
+			a = b;
+			b = siguiente;
+		}
+
 	}
- public void factorial(int num) {
- int resultado = 1;
-     for (int i = 1; i <= num; i++) {
-         resultado *= i;
-     }
-     System.out.println("El factorial de " + num + " es: " + resultado);
- }
-	 
- 
- 
- public boolean esPrimo(int numero) {
-	 if (numero <= 1) {
-         return false;
-     }
-     for (int i = 2; i <= Math.sqrt(numero); i++) {
-         if (numero % i == 0) {
-             return false;
-         }
-     }
-     return true;
-	
-	 
-	 
- }
- 
+
+	/**
+	 * 
+	 * Factorial El factorial de un número se obtiene multiplicando todos los
+	 * números desde 1 hasta el valor del mismo número. Por ejemplo, el factorial de
+	 * 3 se calcula multiplicando 1x2x3, que es 6.
+	 * 
+	 * @param num
+	 */
+
+	public void factorial(int num) {
+		int resultado = 1;
+		for (int i = 1; i <= num; i++) {
+			resultado *= i;
+		}
+		
+	}
+
+	/**
+	 * Número primo Un número es primo cuando sólo es divisible por sí mismo y por
+	 * 1. Un número es divisible por otro cuando al dividir el primero por el
+	 * segundo el resto es cero
+	 * 
+	 * @param numero
+	 * @return
+	 */
+
+	public boolean esPrimo(int numero) {
+		if (numero <= 1) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(numero); i++) {
+			if (numero % i == 0) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
 }
--- PracticaAlgoritmos/src/app/main.java
+++ PracticaAlgoritmos/src/app/main.java
@@ -1,27 +1,73 @@
 package app;
 
+/**
+ * @author VolodymyrDyzhak
+ */
+
+import java.lang.reflect.Array;
+import java.util.Iterator;
+import java.util.Random;
 import Util.Algoritmos;
 
 public class main {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 int numero = 10; 
-		 
-	        Algoritmos algoritmo  = new Algoritmos() {
-			};
-			
-			//LLAmando la funcionFibbonacci
-	        algoritmo.fibonacci(numero);
-	        
-	        //LLamando la finccion factorial
-	        
-	        algoritmo.factorial(numero);
+		/**
+		 * inicializamos el array y el bojeto de la clase Abstracta Objetos
+		 * 
+		 */
+		int[] numeros = new int[10];
+		Algoritmos algoritmo = new Algoritmos() {
+		};
 
-	        
-	        
-	        System.out.println(algoritmo.esPrimo(numero));
+		/**
+		 * con un bucle for recorremos y rellenamos array con la ayuda de la clase
+		 * Random
+		 */
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = new Random().nextInt(6);
+
+		}
+		/**
+		 * bucle for para recorrer el array de numeros y llamar la funcion Fiboncci
+		 * 
+		 */
+		int[] resultadoFibonacci = new int[5];
+		for (int i = 0; i < numeros.length; i++) {
+
+			algoritmo.fibonacci(numeros[i]);
+			/**
+			 * @see # Algoritmo fibonacci
+			 */
+		}
+
+		/**
+		 * bucle for para recorrer el array de numeros y llamar la funcion Factorial
+		 * 
+		 */
+
+		for (int i = 0; i < numeros.length; i++) {
+			
+			algoritmo.factorial(numeros[i]);
+			/**
+			 * @see # Algoritmo factorial
+			 */
+
+		}
+		/**
+		 * bucle for para recorrer el array de numeros y llamar la funcion esPrimo
+		 * 
+		 */
+		for (int i = 0; i < numeros.length; i++) {
+
+			algoritmo.esPrimo(numeros[i]);
+			/**
+			 * @see # Algoritmo esPrimo
+			 */
+		}
+		
+
 	}
 
 }
- 
